17d9d546c1f213cdf957c7d8d057e9d881ac4b3c
sabrinaz35 <sj.zuurbier@gmail.com>
2025-01-09 14:01:40 +0100

    //

---
+++ script 14.00.32.js
@@ -0,0 +1,27 @@
+console.log("java is goed geconnect");
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 4000); 
+}
+
+//er moet nog iets veranderd worden want die code werkt nog niet helemaal, hij gaat namelijk ineens heel snel.
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
